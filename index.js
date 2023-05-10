26aebe3324e89d9ebb8c5f652e12c955e283bafa
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-05-10 12:05:26 +0530

    stripe payment route is done

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
 const orderRoutes = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 app.use(express.json());
 const port = process.env.PORT || 5000;
@@ -17,11 +19,13 @@ mongoose
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(port, () => {
   console.log(`Server Is Running On Port ${port}`);
